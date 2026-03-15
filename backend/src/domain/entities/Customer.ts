22f7f96f502a04dbb9b1dc81a37ba59a1de585ef
luanAfons0 <luan.hmatao@gmail.com>
2026-03-15 01:06:29 -0300

    feat(entities): rename User to Customer and add firstName field

--- backend/src/domain/entities/Customer.ts
+++ backend/src/domain/entities/Customer.ts
@@ -1,14 +1,14 @@
 import Email from "../value-objects/Email";
 import PasswordHash from "../value-objects/PasswordHash";
 
-class User {
+class Customer {
   constructor(
     public readonly id: number,
-    public name: string,
+    public firstName: string,
     public lastName: string,
     public email: Email,
     public password: PasswordHash,
   ) {}
 }
 
-export default User;
+export default Customer;
