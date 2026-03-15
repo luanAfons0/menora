fed5f45d0f1c7428d38c913fc52595e78c9c88c0
luanAfons0 <luan.hmatao@gmail.com>
2026-03-15 01:28:16 -0300

    test(entities): remove infrastructure dependency from Customer spec

--- backend/src/domain/entities/Customer.spec.ts
+++ backend/src/domain/entities/Customer.spec.ts
@@ -1,15 +1,16 @@
-import { describe, it, expect } from "vitest";
-import Customer from "./Customer";
+import { describe, expect, it } from "vitest";
 import Email from "../value-objects/Email";
 import PasswordHash from "../value-objects/PasswordHash";
+import Customer from "./Customer";
 
 describe("Customer", () => {
   it("Should create a valid customer", () => {
+    const id = "mock-uuid-example";
     const email = new Email("test@example.com");
     const passwordHash = PasswordHash.from("somehashedvaluehere");
-    const act = new Customer(1, "Test", "Example", email, passwordHash);
+    const act = new Customer(id, "Test", "Example", email, passwordHash);
 
-    expect(act.id).toBe(1);
+    expect(act.id).toBe(id);
     expect(act.firstName).toBe("Test");
     expect(act.lastName).toBe("Example");
     expect(act.email.value).toBe(email.value);
