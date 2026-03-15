95480d146e0c1aee9a8078544ac3c15795c2bc72
luanAfons0 <luan.hmatao@gmail.com>
2026-03-15 10:19:36 -0300

    feat(mocks): add mock factories for Address and Customer

---
+++ backend/src/domain/entities/Address/Address.mock.ts
@@ -0,0 +1,39 @@
+import { AddressNumber, ZipCode } from "../../value-objects";
+import Address from "./Address";
+
+const ADDRESS_MOCK_DEFAULTS = {
+  id: "mock-uuid",
+  street: "street",
+  city: "city",
+  state: "state",
+  country: "country",
+  zipCode: new ZipCode("12345-678"),
+  number: new AddressNumber("123"),
+};
+
+function createAddressMock(customValues?: Partial<Address>) {
+  const mergedValues = {
+    id: ADDRESS_MOCK_DEFAULTS.id,
+    street: ADDRESS_MOCK_DEFAULTS.street,
+    city: ADDRESS_MOCK_DEFAULTS.city,
+    state: ADDRESS_MOCK_DEFAULTS.state,
+    country: ADDRESS_MOCK_DEFAULTS.country,
+    zipCode: ADDRESS_MOCK_DEFAULTS.zipCode,
+    number: ADDRESS_MOCK_DEFAULTS.number,
+    ...customValues,
+  };
+
+  const address = new Address(
+    mergedValues.id,
+    mergedValues.street,
+    mergedValues.city,
+    mergedValues.state,
+    mergedValues.country,
+    mergedValues.zipCode,
+    mergedValues.number,
+  );
+
+  return address;
+}
+
+export { createAddressMock, ADDRESS_MOCK_DEFAULTS };
---
+++ backend/src/domain/entities/Customer/Customer.mock.ts
@@ -0,0 +1,31 @@
+import { Email, PasswordHash } from "../../value-objects";
+import Customer from "./Customer";
+
+const CUSTOMER_MOCK_DEFAULTS = {
+  id: "mock-uuid-example",
+  email: new Email("test@example.com"),
+  passwordHash: PasswordHash.from("somehashedvaluehere"),
+  firstName: "Test",
+  lastName: "Example",
+};
+
+function createCustomerMock(customValues?: Partial<Customer>) {
+  const mergedValues = {
+    id: CUSTOMER_MOCK_DEFAULTS.id,
+    email: CUSTOMER_MOCK_DEFAULTS.email,
+    password: CUSTOMER_MOCK_DEFAULTS.passwordHash,
+    firstName: CUSTOMER_MOCK_DEFAULTS.firstName,
+    lastName: CUSTOMER_MOCK_DEFAULTS.lastName,
+    ...customValues,
+  };
+
+  return new Customer(
+    mergedValues.id,
+    mergedValues.firstName,
+    mergedValues.lastName,
+    mergedValues.email,
+    mergedValues.password,
+  );
+}
+
+export { createCustomerMock, CUSTOMER_MOCK_DEFAULTS };
