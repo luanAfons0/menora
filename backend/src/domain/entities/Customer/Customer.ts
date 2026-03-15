d3532291c80496912058e365863d78b9d6eb40ec
luanAfons0 <luan.hmatao@gmail.com>
2026-03-15 02:27:57 -0300

    feat(entities): add addAddress and removeAddress methods to Customer

--- backend/src/domain/entities/Customer/Customer.ts
+++ backend/src/domain/entities/Customer/Customer.ts
@@ -1,4 +1,6 @@
 import { Email, PasswordHash } from "../../value-objects";
+import { AddressNotFoundError } from "../../errors";
+import Address from "../Address/Address";
 
 class Customer {
   constructor(
@@ -7,7 +9,18 @@ class Customer {
     public lastName: string,
     public email: Email,
     public password: PasswordHash,
+    public addresses: Array<Address> = [],
   ) {}
+
+  public addAddress(address: Address): void {
+    this.addresses.push(address);
+  }
+
+  public removeAddress(id: string): void {
+    const index = this.addresses.findIndex((address) => address.id === id);
+    if (index === -1) throw new AddressNotFoundError();
+    this.addresses.splice(index, 1);
+  }
 }
 
 export default Customer;
