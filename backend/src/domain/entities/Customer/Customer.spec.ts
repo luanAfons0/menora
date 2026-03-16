583a4f011c926cce4f7980e8719c0170db3ae7dd
luanAfons0 <luan.hmatao@gmail.com>
2026-03-16 17:49:42 -0300

    test(customer): add updateAddress test and refactor addAddress assertions

--- backend/src/domain/entities/Customer/Customer.spec.ts
+++ backend/src/domain/entities/Customer/Customer.spec.ts
@@ -5,6 +5,7 @@ import {
   ADDRESS_MOCK_DEFAULTS,
 } from "../Address/Address.mock";
 import { createCustomerMock, CUSTOMER_MOCK_DEFAULTS } from "./Customer.mock";
+import { AddressNumber, ZipCode } from "../../value-objects";
 
 describe("Customer", () => {
   it("Should create a valid customer", () => {
@@ -24,17 +25,48 @@ describe("Customer", () => {
     act.addAddress(address);
     expect(act.addresses).toHaveLength(1);
 
-    expect(act.addresses[0].id).toBe(ADDRESS_MOCK_DEFAULTS.id);
-    expect(act.addresses[0].street).toBe(ADDRESS_MOCK_DEFAULTS.street);
-    expect(act.addresses[0].city).toBe(ADDRESS_MOCK_DEFAULTS.city);
-    expect(act.addresses[0].state).toBe(ADDRESS_MOCK_DEFAULTS.state);
-    expect(act.addresses[0].country).toBe(ADDRESS_MOCK_DEFAULTS.country);
-    expect(act.addresses[0].zipCode.value).toBe(
+    const firstAddress = act.addresses[0];
+
+    expect(firstAddress.id).toBe(ADDRESS_MOCK_DEFAULTS.id);
+    expect(firstAddress.street).toBe(ADDRESS_MOCK_DEFAULTS.street);
+    expect(firstAddress.city).toBe(ADDRESS_MOCK_DEFAULTS.city);
+    expect(firstAddress.state).toBe(ADDRESS_MOCK_DEFAULTS.state);
+    expect(firstAddress.country).toBe(ADDRESS_MOCK_DEFAULTS.country);
+    expect(firstAddress.zipCode.value).toBe(
       ADDRESS_MOCK_DEFAULTS.zipCode.value,
     );
-    expect(act.addresses[0].number.value).toBe(
-      ADDRESS_MOCK_DEFAULTS.number.value,
-    );
+    expect(firstAddress.number.value).toBe(ADDRESS_MOCK_DEFAULTS.number.value);
+  });
+
+  it("Should update a user address", () => {
+    const act = createCustomerMock();
+    const address = createAddressMock();
+
+    act.addAddress(address);
+    expect(act.addresses).toHaveLength(1);
+
+    const changedNumber = new AddressNumber("321");
+    const changedZipCode = new ZipCode("98765-432");
+
+    act.updateAddress({
+      id: address.id,
+      city: "changed-city",
+      country: "changed-country",
+      number: changedNumber,
+      state: "changed-state",
+      street: "changed-street",
+      zipCode: changedZipCode,
+    });
+
+    const firstAddress = act.addresses[0];
+
+    expect(firstAddress.id).toBe(address.id);
+    expect(firstAddress.street).toBe("changed-street");
+    expect(firstAddress.city).toBe("changed-city");
+    expect(firstAddress.state).toBe("changed-state");
+    expect(firstAddress.country).toBe("changed-country");
+    expect(firstAddress.zipCode.value).toBe(changedZipCode.value);
+    expect(firstAddress.number.value).toBe(changedNumber.value);
   });
 
   it("Should remove a address to the customer", () => {
