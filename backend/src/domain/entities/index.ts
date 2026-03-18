076f7676ddd36671431a6d48ed3917c772a3e549
luanAfons0 <luan.hmatao@gmail.com>
2026-03-18 09:36:10 -0300

    chore(entities/errors): export Product entity and new product errors

--- backend/src/domain/entities/index.ts
+++ backend/src/domain/entities/index.ts
@@ -1,3 +1,4 @@
-export { default as Customer } from "./Customer/Customer";
 export { default as Address } from "./Address/Address";
+export { default as Customer } from "./Customer/Customer";
+export { default as Product } from "./Product/Product";
 export { default as User } from "./User/User";
