bfe2634099ab42e12ce094f2fc96b596f5009eef
luanAfons0 <luan.hmatao@gmail.com>
2026-03-28 14:40:01 -0300

    refactor(user): encapsulate VO fields with private backing and update methods

--- backend/src/domain/entities/User/User.ts
+++ backend/src/domain/entities/User/User.ts
@@ -6,9 +6,25 @@ class User {
     public readonly id: Uuid,
     public firstName: string,
     public lastName: string,
-    public readonly email: Email,
-    public readonly password: PasswordHash,
+    private _email: Email,
+    private _password: PasswordHash,
   ) {}
+
+  get email() {
+    return this._email;
+  }
+
+  public updateEmail(email: Email) {
+    this._email = email;
+  }
+
+  get password() {
+    return this._password;
+  }
+
+  public updatePassword(password: PasswordHash) {
+    this._password = password;
+  }
 }
 
 export default User;
